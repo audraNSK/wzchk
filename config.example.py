fbb60ce2e1e2761822be1eddd2f45b717a948c14
Ričardas Sabaliauskas <ricardas.sabaliauskas@ittc.vu.lt>
2017-01-23 10:35:13 +0200

    Config removed from repository

---
+++ config.example.py
@@ -0,0 +1,15 @@
+# url list base location
+list_url_base = ''
+
+# hostname separator
+hostname_separator = '_'
+
+# zabbix server url
+zabbix_api_url = ''
+
+# api credentials
+zabbix_username = ''
+zabbix_password = ''
+
+# group id
+zabbix_group_id = ''
